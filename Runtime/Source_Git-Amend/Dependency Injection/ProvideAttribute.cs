43e2e702b0ee7a338a0185d0765f6b2cb74062d7
Lucas Ackman <AckmanLD@gmail.com>
2025-01-16 01:59:26 -0600

    Removed unrelated files

--- Runtime/Source_Git-Amend/Dependency Injection/ProvideAttribute.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-
-namespace Kickstarter.DependencyInjection
-{
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Method | AttributeTargets.Property)]
-    public sealed class ProvideAttribute : Attribute { }
-}
